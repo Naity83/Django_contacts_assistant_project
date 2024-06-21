2e5f2136cd13efcd9b5d559eacb289b77a352b02
Nataliya Khomenko <NataliaHomenko83@gmail.com>
2024-06-21 11:09:08 +0300

    contacts

--- contacts_assistant/contacts/models.py
+++ contacts_assistant/contacts/models.py
@@ -1,17 +1,18 @@
+# models.py
 from django.db import models
 from django.core.validators import RegexValidator, EmailValidator
 from datetime import date, timedelta
 from django.contrib.auth.models import User
 
 class Contact(models.Model):
-    full_name = models.CharField(max_length=200)    
+    full_name = models.CharField(max_length=200)
     address = models.CharField(max_length=255)
     phone_number = models.CharField(
         max_length=15,
         validators=[RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")]
     )
     email = models.EmailField(validators=[EmailValidator(message="Enter a valid email address.")])
-    birthday = models.DateField()
+    birthday = models.DateField(null=True, blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
 
     def __str__(self):
@@ -22,4 +23,11 @@ class Contact(models.Model):
         today = date.today()
         upcoming_date = today + timedelta(days=days)
         return Contact.objects.filter(birthday__month=upcoming_date.month, birthday__day=upcoming_date.day)
+
+    @staticmethod
+    def get_upcoming_birthdays(days):
+        today = date.today()
+        upcoming_date = today + timedelta(days=days)
+        return Contact.objects.filter(birthday__gte=today, birthday__lte=upcoming_date)
+
 
--- contacts_assistant/contacts/views.py
+++ contacts_assistant/contacts/views.py
@@ -1,4 +1,59 @@
-from django.shortcuts import render
+import logging
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Contact
+from .forms import ContactForm
+
+logger = logging.getLogger(__name__)
 
 def main(request):
     return render(request, 'contacts/index.html')
+
+def contact_list(request):
+    contacts = Contact.objects.filter(user=request.user)
+    return render(request, 'contacts/contact_list.html', {'contacts': contacts})
+
+import logging
+from django.shortcuts import render, redirect
+from .forms import ContactForm
+
+logger = logging.getLogger(__name__)
+
+def contact_create(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            contact = form.save(commit=False)
+            contact.user = request.user
+            contact.save()
+            logger.info('Contact created successfully')
+            return redirect('contacts:contact_list')
+        else:
+            logger.error('Form is not valid')
+            logger.error(form.errors)
+    else:
+        form = ContactForm()
+    return render(request, 'contacts/contact_form.html', {'form': form})
+
+
+def contact_update(request, pk):
+    contact = get_object_or_404(Contact, pk=pk)
+    if request.method == 'POST':
+        form = ContactForm(request.POST, instance=contact)
+        if form.is_valid():
+            form.save()
+            return redirect('contacts:contact_list')
+    else:
+        form = ContactForm(instance=contact)
+    return render(request, 'contacts/contact_form.html', {'form': form})
+
+def contact_delete(request, pk):
+    contact = get_object_or_404(Contact, pk=pk)
+    if request.method == 'POST':
+        contact.delete()
+        return redirect('contacts:contact_list')
+    return render(request, 'contacts/contact_confirm_delete.html', {'contact': contact})
+
+def upcoming_birthdays(request, days):
+    upcoming_contacts = Contact.get_upcoming_birthdays(days)
+    return render(request, 'contacts/upcoming_birthdays.html', {'upcoming_contacts': upcoming_contacts})
+
