d21e75d16e24437781d5751d1bcd0218c539d38d
basilegupov <basilegupov@gmail.com>
2024-06-24 10:27:31 +0300

    merge with new main

--- contacts_assistant/files/urls.py
+++ contacts_assistant/files/urls.py
@@ -7,5 +7,5 @@ app_name = "files"
 urlpatterns = [
     path('', views.index, name='home'),
     path('all_my_files/', views.all_files, name='all_my_files'),
-    path('upload/', views.upload, name='upload')
+    path('upload/', views.upload, name='upload_file')
 ]
