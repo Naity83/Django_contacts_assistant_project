0699e65ec612916ec820dca76e9157634ebf1950
HelenNad <htkachenko88@gmail.com>
2024-06-24 10:22:05 +0200

    notes

--- contacts_assistant/contacts_assistant/settings.py
+++ contacts_assistant/contacts_assistant/settings.py
@@ -11,13 +11,14 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-import os
-from dotenv import load_dotenv
+import environ
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env()
+environ.Env.read_env(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -83,12 +84,12 @@ WSGI_APPLICATION = 'contacts_assistant.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
+        'ENGINE': env('DB_ENGINE'),
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT'),
     }
 }
 
--- contacts_assistant/notes/views.py
+++ contacts_assistant/notes/views.py
@@ -1,3 +1,62 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+
+from .forms import TagForm, NoteForm
+from .models import Tag, Note
+
 
 # Create your views here.
+def notes_main(request):
+    notes = Note.objects.all()
+    return render(request, 'notes/note_base.html', {"notes": notes})
+
+
+def add_tag(request):
+    tags = Tag.objects.all()
+    if request.method == 'POST':
+        form = TagForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(to='notes:notes_main')
+        else:
+            return render(request, 'notes/tag_add.html', {"tags": tags, 'form': form})
+
+    return render(request, 'notes/tag_add.html', {"tags": tags, 'form': TagForm()})
+
+
+def note_add(request):
+    tags = Tag.objects.all()
+    if request.method == 'POST':
+        form = NoteForm(request.POST)
+        if form.is_valid():
+            new_note = form.save()
+            choice_tags = Tag.objects.filter(name__in=request.POST.getlist('tags'))
+            for tag in choice_tags.iterator():
+                new_note.tags.add(tag)
+            return redirect(to='notes:notes_main')
+        else:
+            return render(request, 'notes/note_add.html', {"tags": tags, 'form': form})
+
+    return render(request, 'notes/note_add.html', {"tags": tags, 'form': NoteForm()})
+
+
+def delete_note(request, pk):
+    note = Note.objects.filter(pk=pk)
+    if request.method == 'POST':
+        note.delete()
+        return redirect(to='notes:notes_main')
+
+
+def note_update(request, pk):
+    tags = Tag.objects.all()
+    note = get_object_or_404(Note, pk=pk)
+    if request.method == 'POST':
+        form = NoteForm(request.POST, instance=note)
+        if form.is_valid():
+            update_note = form.save()
+            choice_tags = Tag.objects.filter(name__in=request.POST.getlist('tags'))
+            for tag in choice_tags.iterator():
+                update_note.tags.add(tag)
+            return redirect('notes:notes_main')
+    else:
+        form = NoteForm(instance=note)
+    return render(request, 'notes/note_update.html', {"tags": tags, 'form': form})
