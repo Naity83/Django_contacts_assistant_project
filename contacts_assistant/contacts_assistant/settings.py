f105fff20ad9ccd7b26a33dc5fa6ebb8b057c35d
HelenNad <htkachenko88@gmail.com>
2024-06-25 11:27:16 +0200

    change settings

--- contacts_assistant/contacts_assistant/settings.py
+++ contacts_assistant/contacts_assistant/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 
-
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ot3in7dh5$pb+xpd6-!%rfllwnna&_zjr-8&ib0j24gx@^n)c_'
+SECRET_KEY = os.getenv('SECRET_KEY')   # 'django-insecure-ot3in7dh5$pb+xpd6-!%rfllwnna&_zjr-8&ib0j24gx@^n)c_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
