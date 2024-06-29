7c5468ec543b58e2a3428e9b75ace428c8ff8b2c
basilegupov <basilegupov@gmail.com>
2024-06-29 21:01:15 +0300

    deploy v10

--- contacts_assistant/contacts_assistant/settings.py
+++ contacts_assistant/contacts_assistant/settings.py
@@ -21,22 +21,19 @@ load_dotenv(dotenv_path)
 
 load_dotenv()
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')   # 'django-insecure-ot3in7dh5$pb+xpd6-!%rfllwnna&_zjr-8&ib0j24gx@^n)c_'
+SECRET_KEY = os.getenv('SECRET_KEY')  # 'django-insecure-ot3in7dh5$pb+xpd6-!%rfllwnna&_zjr-8&ib0j24gx@^n)c_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -47,11 +44,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'contacts_assistant.contacts',
-    'contacts_assistant.notes',
-    'contacts_assistant.users',
-    'contacts_assistant.my_files',
-    'contacts_assistant.news',
+    'contacts_assistant.contacts_assistant.contacts',
+    'contacts_assistant.contacts_assistant.notes',
+    'contacts_assistant.contacts_assistant.users',
+    'contacts_assistant.contacts_assistant.my_files',
+    'contacts_assistant.contacts_assistant.news',
     # 'cloudinary',
     # 'cloudinary_storage',
 ]
@@ -66,7 +63,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'contacts_assistant.urls'
+ROOT_URLCONF = 'contacts_assistant.contacts_assistant.urls'
 
 TEMPLATES = [
     {
@@ -85,7 +82,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'contacts_assistant.wsgi.application'
-
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
@@ -134,7 +130,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
@@ -148,8 +143,6 @@ USE_TZ = True
 
 USE_L10N = False
 
-
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
@@ -157,11 +150,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-       os.path.join(BASE_DIR, 'static'),
-   ]
+    os.path.join(BASE_DIR, 'static'),
+]
 
-
-LOGIN_URL = "/users/signin"    
+LOGIN_URL = "/users/signin"
 LOGIN_REDIRECT_URL = "/"
 
 # Default primary key field type
@@ -171,7 +163,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
-
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.getenv('CLD_NAME'),
     'API_KEY': os.getenv('CLD_API_KEY'),
@@ -179,8 +170,6 @@ CLOUDINARY_STORAGE = {
 }
 
 CLOUDINARY_URL = f"cloudinary://{CLOUDINARY_STORAGE['API_KEY']}:{CLOUDINARY_STORAGE['API_SECRET']}@{CLOUDINARY_STORAGE['CLOUD_NAME']}"
-
-
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.meta.ua'
@@ -191,4 +180,3 @@ EMAIL_USE_TLS = False
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
-
