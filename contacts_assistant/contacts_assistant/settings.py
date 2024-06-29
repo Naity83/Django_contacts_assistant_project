e3020cda21b6dfd09005bd61185e99f808f360da
basilegupov <basilegupov@gmail.com>
2024-06-29 19:40:33 +0300

    deploy v3

--- contacts_assistant/contacts_assistant/settings.py
+++ contacts_assistant/contacts_assistant/settings.py
@@ -153,7 +153,13 @@ USE_L10N = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+       os.path.join(BASE_DIR, 'static'),
+   ]
+
 
 LOGIN_URL = "/users/signin"    
 LOGIN_REDIRECT_URL = "/"
